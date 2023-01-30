437f14669ecca7b850235336705efefb4bd4cf73
Benjamin Fildier <bfildier.work@gmail.com>
2023-01-30 11:20:43 +0100

    fix showData in invlog axes and hatching

--- src/plot1D.py
+++ src/plot1D.py
@@ -79,55 +79,90 @@ def setYaxisIL(ax,ranks):
     ax.set_yticks(yticks)
     ax.set_yticklabels(yticklabels)
     
-def showData(ax,values,axisIL='x',**kwargs):
+def showData(ax,ranks,values,axisIL='x',**kwargs):
     """Show data as it is, regardless of preset frame and ticks"""
 
     if axisIL == 'x':
         
-        dum_x = np.linspace(0,1,len(values))
+        x = 1/(1-ranks/100.)
         # show
-        h = ax.plot(dum_x,values,**kwargs)
+        h = ax.plot(x,values,**kwargs)
         # be careful that the x bounds are precisely the same as the background frame
         ax.margins(x=0)
+        # log
+        ax.set_xscale ('log')
         # remove ticks
         ax.set_xticks([])
+        ax.set_xticks([], minor=True)
         
         return h
 
     elif axisIL == 'y':
         
-        dum_y = np.linspace(0,1,len(values))
+        y = 1/(1-ranks/100.)
         # show
-        h = ax.plot(values,dum_y,**kwargs)
+        h = ax.plot(values,y,**kwargs)
         # be careful that the y bounds are precisely the same as the background frame
         ax.margins(y=0)
+        # log
+        ax.set_yscale ('log')
         # remove ticks
         ax.set_yticks([])
+        ax.set_yticks([], minor=True)
 
         return h
+        return h
+
+def subplotRanksILog(ax,ranks,y,sl=slice(None,None),col=None,ltype=None,linewidth=None,alpha=None,
+    labels=None,renameX=True,offset=0):
+    """RECODE THAT ONE to combine steps"""
     
-def addXHatch(ax,x,i_xlim,color='gray',hatch='//',
+    #- duplicate axes for rescaling frame
+    ax_frame = ax.twiny()
+    
+    #- set frame
+    setXaxisIL(ax_frame,ranks)
+    
+    #- show data
+    showData(ax,ranks,y,axisIL='x')
+    
+    # plot
+    if isinstance(y,list):
+        for i in range(len(y)):
+            lab = None
+            if labels is not None:
+                lab = labels[i]
+            lt = ltype[i] if ltype is not None else '-'
+            a = alpha[i] if alpha is not None else 1
+            c = col[i] if col is not None else 1
+            lw = linewidth[i] if linewidth is not None else 1.5
+            showData(ax,ranks,y,axisIL='x',c=c,alpha=a,linestyle=lt,linewidth=lw,label=lab)
+    else:
+        showData(ax,ranks,y,axisIL='x',c=col,alpha=alpha,linestyle=ltype,linewidth=linewidth,label=labels)
+
+
+def addXHatch(ax,ranks,i_xlim,color='gray',hatch='//',
     alpha=1,fill=False,**kwargs):
     """Add vertical shading"""
 
-    dum_x = np.linspace(0,1,len(x))
+    x = 1/(1-ranks/100.)
     
-    ax.add_patch(Polygon([[dum_x[i_xlim[0]], ax.get_ylim()[0]],\
-                          [dum_x[i_xlim[1]], ax.get_ylim()[0]],\
-                          [dum_x[i_xlim[1]], ax.get_ylim()[1]],\
-                          [dum_x[i_xlim[0]], ax.get_ylim()[1]]],\
+    ax.add_patch(Polygon([[x[i_xlim[0]], ax.get_ylim()[0]],\
+                          [x[i_xlim[1]], ax.get_ylim()[0]],\
+                          [x[i_xlim[1]], ax.get_ylim()[1]],\
+                          [x[i_xlim[0]], ax.get_ylim()[1]]],\
                           closed=True, fill=fill, hatch=hatch,linewidth=0,
                           color=color,alpha=alpha,**kwargs))
     
-def addYHatch(ax,y,i_ylim,color='gray',hatch='//',
+def addYHatch(ax,ranks,i_ylim,color='gray',hatch='//',
     alpha=1,fill=False,**kwargs):
     """Add horizontal shading"""
 
-    dum_y = np.linspace(0,1,len(y))
+    y = 1/(1-ranks/100.)
     
-    ax.add_patch(Polygon([[ax.get_xlim()[0],dum_y[i_ylim[0]]],\
-                          [ax.get_xlim()[0],dum_y[i_ylim[1]]],\
-                          [ax.get_xlim()[1],dum_y[i_ylim[1]]],\
-                          [ax.get_xlim()[1],dum_y[i_ylim[0]]]],\
+    ax.add_patch(Polygon([[ax.get_xlim()[0],y[i_ylim[0]]],\
+                          [ax.get_xlim()[0],y[i_ylim[1]]],\
+                          [ax.get_xlim()[1],y[i_ylim[1]]],\
+                          [ax.get_xlim()[1],y[i_ylim[0]]]],\
                           closed=True, fill=fill, hatch=hatch,linewidth=0,
                           color=color,alpha=alpha,**kwargs))
